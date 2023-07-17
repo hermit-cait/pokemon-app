3a4ce8664f617cd1e0491e3d38198edef6d8dd84
hermit-cait <63665275+hermit-cait@users.noreply.github.com>
2023-07-17 11:28:56 +0200

    Display API data in the DOM

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")
-.then(res => {
-  return res.json()
-})
-.then(data => console.log(data))
-.catch(error => console.log("ERROR"))
+getData();
+
+  async function getData(){
+       const response= await fetch('https://pokeapi.co/api/v2/pokemon/magikarp')
+       console.log(response);
+       const data= await response.json();
+       length=data.types.length;
+       var temp="";
+       temp+="<tr>";
+       temp+="<td>"+data.name+"</td>";
+       temp+="<td><img src="+data.sprites.front_default+"></td>";
+       for(i=0;i<length;i++)
+       {          
+         temp+="<td>"+data.types[i].type.name+"</td>";
+       }
+
+    document.getElementById("data").innerHTML=temp;
+     }
