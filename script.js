74bf3b0704c58b7747646a5fb320ce0147b7e958
hermit-cait <63665275+hermit-cait@users.noreply.github.com>
2023-07-17 11:32:24 +0200

    Make code more readable

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 getData();
 
-  async function getData(){
-       const response= await fetch('https://pokeapi.co/api/v2/pokemon/magikarp')
-       console.log(response);
-       const data= await response.json();
-       length=data.types.length;
-       var temp="";
-       temp+="<tr>";
-       temp+="<td>"+data.name+"</td>";
-       temp+="<td><img src="+data.sprites.front_default+"></td>";
-       for(i=0;i<length;i++)
-       {          
-         temp+="<td>"+data.types[i].type.name+"</td>";
-       }
-
-    document.getElementById("data").innerHTML=temp;
-     }
+async function getData() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/magikarp')
+  const data = await response.json();
+  length = data.types.length;
+  var template = "";
+  template += "<tr>";
+  template += "<td>" + data.name + "</td>";
+  template += "<td><img src=" + data.sprites.front_default + "></td>";
+  for (i=0;i<length;i++)
+  {          
+    template += "<td>" + data.types[i].type.name + "</td>";
+  }
+  document.getElementById("data").innerHTML = template;
+}
