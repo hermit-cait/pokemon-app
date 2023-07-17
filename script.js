c509fa8dd47e855e9aae847a1160a6f9dc23c9d6
hermit-cait <63665275+hermit-cait@users.noreply.github.com>
2023-07-17 14:34:06 +0200

    Add random Pokemon generator

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 getData();
 
 async function getData() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/magikarp')
-  const data = await response.json();
-  length = data.types.length;
-  var template = "";
-  template += "<tr>";
-  template += "<td>" + data.name + "</td>";
-  template += "<td><img src=" + data.sprites.front_default + "></td>";
-  for (i=0;i<length;i++)
-  {          
-    template += "<td>" + data.types[i].type.name + "</td>";
+  try {
+    randomPokemon = Math.floor(Math.random() * 387);
+    randomURL = "https://pokeapi.co/api/v2/pokemon/"
+    randomURL += randomPokemon
+    const response = await fetch(randomURL)
+    const data = await response.json();
+
+    length = data.types.length;
+    template = "";
+    template += "<td>name: " + data.name + "</td>";
+    template += "<td><img src=" + data.sprites.front_default + "></td>";
+    types = "";
+    types += "<td>type: "
+
+    for (i = 0; i < length; i++) {
+      types += data.types[i].type.name;
+      if (i < length - 1) {
+        types += ", "
+      }
+    }
+
+    types += "</td>"
+    document.getElementById("data").innerHTML = template + types;
+    
+  } catch (error) {
+    console.log(error)
+    var template = "";
+    template += "<td>404 ERROR NOT FOUND</td>";
+    document.getElementById("data").innerHTML = template;
   }
-  document.getElementById("data").innerHTML = template;
 }
